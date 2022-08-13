d19b24bcaca7d61c120797eee2780a3fbd188caf
suraj7076 <surajmaity981@gmail.com>
2022-08-13 08:27:38 +0530

    Initial Version

---
+++ src/LeetCode/LCSBST.java
@@ -0,0 +1,25 @@
+package LeetCode;
+
+import java.util.ArrayList;
+
+public class LCSBST {
+    //https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root == null || root == p || root == q){
+            return root;
+        }
+
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+        if(left == null){
+            return right;
+        }
+        else if(right == null){
+            return left;
+        }
+        else {
+            return root;
+        }
+    }
+}
--- src/Practice/Program.java
+++ src/Practice/Program.java
@@ -1,49 +1,19 @@
 package Practice;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Program {
     public static void main(String[] args) {
-
-    }
-    static boolean isValidBST(TreeNode root) {
-        boolean result = false;
-
+        ArrayList<TreeNode> pPath = new ArrayList<>();
+        pPath.add(new TreeNode(32));
+        System.out.println(pPath);
 
 
-        return result;
-    }
-    static TreeNode rec(TreeNode root){
-        if(root == null){
-            return null;
-        }
-        return root;
-    }
 
 
-    //brute-force using inorder traversal
-    public boolean isValidBST2(TreeNode root) {
-        ArrayList<Integer> list = new ArrayList<>();
-        inOrder2(root, list);
-        for(int i = 0; i< list.size()-1; i++){
-            if(list.get(i) >= list.get(i+1)){
-                return false;
-            }
-        }
-        return true;
-
-    }
-
-    static void inOrder2(TreeNode root, ArrayList<Integer> list){
-        if(root == null){
-            return;
-        }
-        inOrder2(root.left, list);
-        list.add(root.val);
-        inOrder2(root.right, list);
     }
 }
-
 
 class TreeNode {
     int val;
