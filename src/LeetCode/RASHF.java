91dd918230ac0be6c83103952ef967fe6fd1f9ac
suraj7076 <surajmaity981@gmail.com>
2022-08-18 17:54:17 +0530

    LeetCode updated

--- src/LeetCode/RASHF.java
+++ src/LeetCode/RASHF.java
@@ -9,6 +9,25 @@ public class RASHF {
         System.out.println(minSetSize(arr));
     }
 
+
+    //Time: O(N)      Space: O(N)
+    public int minSetSize2(int[] arr) {
+        int n = arr.length;
+        HashMap<Integer, Integer> cnt = new HashMap<>();
+        for (int x : arr) cnt.put(x, cnt.getOrDefault(x, 0) + 1);
+
+        int[] counting = new int[n + 1];
+        for (int freq : cnt.values()) ++counting[freq];
+
+        int ans = 0, removed = 0, half = n / 2, freq = n;
+        while (removed < half) {
+            ans += 1;
+            while (counting[freq] == 0) --freq;
+            removed += freq;
+            --counting[freq];
+        }
+        return ans;
+    }
     //Time: O(NlogN)
     //Space: O(N)
     static int minSetSize(int[] arr) {
