40d9dcd396e2ca6d5b518d1a627aa174dba2243d
suraj7076 <surajmaity981@gmail.com>
2022-10-10 20:06:07 +0530

    LeetCode updated

--- src/LeetCode/DBT.java
+++ src/LeetCode/DBT.java
@@ -4,18 +4,17 @@ public class DBT {
     //https://leetcode.com/problems/diameter-of-binary-tree/
 
     public int diameterOfBinaryTree1(TreeNode root) {
-        int[] diameter = new int[1];
-        diameterCalculate1(root, diameter);
-        return diameter[0];
+        int[] max = new int[1];
+        diameter(root, max);
+        return max[0];
     }
-    public int diameterCalculate1(TreeNode root, int[] diameter){
+    public int diameter(TreeNode root, int[] max){
         if(root == null) return 0;
 
-        int left = diameterCalculate1(root.left, diameter);
-        int right = diameterCalculate1(root.right, diameter);
+        int left = diameter(root.left, max);
+        int right = diameter(root.right, max);
 
-        diameter[0] = Math.max(diameter[0], (left+right));
-
+        max[0] = Math.max(max[0], (left+right));
         return 1 + Math.max(left, right);
     }
 }
