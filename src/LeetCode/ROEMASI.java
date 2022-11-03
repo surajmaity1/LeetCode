3f3f26cd104481e6d68c7eed9d318e202b63be46
surajmaity1 <surajmaity981@gmail.com>
2022-11-03 16:13:40 +0530

    Leetcode updated

---
+++ src/LeetCode/ROEMASI.java
@@ -0,0 +1,22 @@
+package LeetCode;
+
+public class ROEMASI {
+    //https://leetcode.com/problems/remove-one-element-to-make-the-array-strictly-increasing/description/
+    public boolean canBeIncreasing(int[] nums) {
+        int n = nums.length;
+        int count = 0;
+
+        for(int i = 1; i < n; i++){
+            //check current element <= prev element
+            if (nums[i] <= nums[i-1]){
+                count++;
+
+                //check current element <= second prev element
+                if (i > 1 && nums[i] <= nums[i-2]){
+                    nums[i] = nums[i-1];
+                }
+            }
+        }
+        return count <= 1;
+    }
+}
