aa6b7188e85d919f3c59579f98fd7e2f33c2b634
surajmaity1 <surajmaity981@gmail.com>
2022-12-30 11:41:42 +0530

    LeetCode Solved

---
+++ src/LeetCode/LLW.java
@@ -0,0 +1,10 @@
+package LeetCode;
+
+public class LLW {
+    static int lengthOfLastWord(String s) {
+        if (s == null || s.length() == 0) return 0;
+
+        int len = 0;
+        return len;
+    }
+}
--- src/Practice/Program.java
+++ src/Practice/Program.java
@@ -5,15 +5,9 @@ import java.util.*;
 public class Program{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter Your Number: ");
-        int n = sc.nextInt();
-        int res = 1;
+        float a = 3f;
+        System.out.println(a);
 
-        for (int i = 1; i <= 10; i++) {
-            System.out.print(n + " * " + i + " = " );
-            res = n * i;
-            System.out.println(res);
-        }
     }
 
 }
