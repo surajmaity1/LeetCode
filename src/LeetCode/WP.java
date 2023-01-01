66b2d80c3da1407020f5003b6d03459dd4b4d9d9
surajmaity1 <surajmaity981@gmail.com>
2023-01-01 17:58:19 +0530

    LeetCode Solved

---
+++ src/LeetCode/WP.java
@@ -0,0 +1,27 @@
+package LeetCode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class WP {
+    public boolean wordPattern1(String pattern, String s) {
+        String[] words = s.split(" ");
+
+        if (words.length != pattern.length()) return false;
+
+        Map<Character, String> charToWord = new HashMap<>();
+        Map<String, Character> wordToChar = new HashMap<>();
+
+        for (int i = 0; i < pattern.length(); i++) {
+            char c = pattern.charAt(i);
+            String w = words[i];
+            if (charToWord.containsKey(c) && !charToWord.get(c).equals(w))
+                return false;
+            if (wordToChar.containsKey(w) && wordToChar.get(w) != c)
+                return false;
+            charToWord.put(c, w);
+            wordToChar.put(w, c);
+        }
+        return true;
+    }
+}
