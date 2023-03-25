c5735bc2b2d2e226fb343f568aba361bce3f1f8f
suraj <surajmaity981@gmail.com>
2023-03-25 15:11:09 +0530

    LeetCode Updated

---
+++ src/LeetCode/IQS.java
@@ -0,0 +1,52 @@
+package LeetCode;
+
+import java.util.Stack;
+
+public class IQS {
+    // NOTE: INSERT EFFICIENT
+    class QueueUsingStack {
+        private Stack<Integer> first;
+        private Stack<Integer> second;
+
+        public QueueUsingStack() {
+            first = new Stack<>();
+            second = new Stack<>();
+        }
+
+        public void add(int item) {
+            first.push(item);
+        }
+
+        public int remove() throws Exception {
+            while (!first.isEmpty()) {
+                second.push(first.pop());
+            }
+            int removed = second.pop();
+            while (!second.isEmpty()) {
+                first.push(second.pop());
+            }
+            return removed;
+        }
+
+        public int peek() throws Exception {
+            while (!first.isEmpty()) {
+                second.push(first.pop());
+            }
+
+            int peeked = second.peek();
+
+            while (!second.isEmpty()) {
+                first.push(second.pop());
+            }
+            return peeked;
+        }
+
+        public boolean isEmpty() {
+            return first.isEmpty();
+        }
+
+    }
+
+    // NOTE: DELETE EFFICIENT
+
+}
